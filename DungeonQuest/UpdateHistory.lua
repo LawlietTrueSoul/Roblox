b18efc6acf09549c46fffc6c010e478c5165fddb
LawlietTrueSoul <lawliet.truesoul@gmail.com>
2024-11-11 21:57:47 +0700

    Update UpdateHistory.lua

--- DungeonQuest/UpdateHistory.lua
+++ DungeonQuest/UpdateHistory.lua
@@ -1,5 +1,12 @@
 return {
     {
+        Title = "November 1, 2024",
+        Content = {
+            "- First Demo"
+        },
+    },
+    
+    {
         Title = "November 11, 2024",
         Content = {
             "- Release"
